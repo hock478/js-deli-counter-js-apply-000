9fcbaeb9bf08c39ff13399e411401e95c07edc18
Dawit Gizaw <gizawdawit123@gmail.com>
2020-01-30 22:21:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var katzDeliLine = [];
-var i = 0 
-function takeANumber(line,name){
-  return "Welcome, " + name + ". You are number " + (line.push(name)) + " in line."
+var i = 0;
+// //function takeANumber(line,name){
+//   return "Welcome, " + name + ". You are number " + (line.push(name)) + " in line."
+// }
+function takeANumber(line){
+  // i = 1, line = [1],  
+  return "Welcome, you are ticket # " + (line.push(i++)) // i = 2 line = [1,2] 
+
 }
 
 function nowServing(line){
@@ -10,8 +15,7 @@ function nowServing(line){
   if(line[0] === undefined){
     return "There is nobody waiting to be served!"
   }else{
-    value = line[0]
-    line.shift()
+    value = line.shift()
     return "Currently serving " + value + "."
   }
 }
